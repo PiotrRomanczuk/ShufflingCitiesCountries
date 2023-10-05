579e8b1f413dec33faf1fda5c137638123f30962
PiotrRomanczuk <77811265+PiotrRomanczuk@users.noreply.github.com>
2023-10-05 21:39:54 +0200

    Removing buttons on wrong pair selection

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,48 @@
-const capitolCountries = [
-	{
-		country: 'United States',
-		capitol: 'Washington',
-	},
-	{
-		country: 'Germany',
-		capitol: 'Berlin',
-	},
-	{
-		country: 'Poland',
-		capitol: 'Warsaw',
-	},
-];
+import { useState } from 'react';
+import './App.css';
+
+type ButtonState = 'DEFAULT' | 'SELECTED' | 'WRONG';
+const blueColor = '#009BFF';
+
+function CountryCapitalGame({ data }: { data: Record<string, string> }) {
+	const countries = Object.keys(data);
+	const capitols = Object.values(data);
+	const options = [...countries, ...capitols];
+
+	const [colorMap, setColorMap] = useState<Record<string, ButtonState>>({});
+
+	options.sort(() => Math.random() - 0.5);
+
+	return (
+		<>
+			{options.map((option, idx) => {
+				return (
+					<button
+						className={colorMap[option] === 'SELECTED' ? 'selected' : ''}
+						onClick={() => {
+							setColorMap({
+								...colorMap,
+								[option]: 'SELECTED',
+							});
+						}}
+						key={idx}
+					>
+						{option}
+					</button>
+				);
+			})}
+		</>
+	);
+}
 
 function App() {
 	return (
-		<>
-			<h1>Shuffling game!!!</h1>
-			{capitolCountries.map((item, index) => (
-				<div key={index}>
-					<p>Country: {item.country}</p>
-					<p>Capital: {item.capitol}</p>
-				</div>
-			))}
-		</>
+		<div className='App'>
+			<h1>Country and Capital game!!!</h1>
+			<div className='game'>
+				<CountryCapitalGame data={{ Germany: 'Berlin', Azerbeijan: 'Baku' }} />
+			</div>
+		</div>
 	);
 }
 
