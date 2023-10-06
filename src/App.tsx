93a7e92457e07c33a6052a1c97c3a82a29ffee6b
PiotrRomanczuk <77811265+PiotrRomanczuk@users.noreply.github.com>
2023-10-06 13:07:10 +0200

    Game finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,91 @@
+/* eslint-disable no-debugger */
+/* eslint-disable no-mixed-spaces-and-tabs */
 import { useState } from 'react';
 import './App.css';
 
 type ButtonState = 'DEFAULT' | 'SELECTED' | 'WRONG';
-const blueColor = '#009BFF';
+
+type Option = {
+	value: string;
+	state: ButtonState;
+};
 
 function CountryCapitalGame({ data }: { data: Record<string, string> }) {
 	const countries = Object.keys(data);
-	const capitols = Object.values(data);
-	const options = [...countries, ...capitols];
+	const capitals = Object.values(data);
 
-	const [colorMap, setColorMap] = useState<Record<string, ButtonState>>({});
+	const [options, setOptions] = useState<Option[]>(
+		[...countries, ...capitals]
+			.sort(() => Math.random() - 0.5)
+			.map((value) => ({
+				value,
+				state: 'DEFAULT',
+			}))
+	);
 
-	options.sort(() => Math.random() - 0.5);
+	const [selected, setSelected] = useState<Option>();
+	const isGameOver = options.length === 0;
+
+	if (isGameOver) {
+		return <h1 className='green'>Congratulations</h1>;
+	}
 
 	return (
 		<>
-			{options.map((option, idx) => {
+			{options.map((option) => {
 				return (
 					<button
-						className={colorMap[option] === 'SELECTED' ? 'selected' : ''}
+						className={
+							option.state === 'SELECTED'
+								? 'selected'
+								: option.state === 'WRONG'
+								? 'wrong'
+								: ''
+						}
+						key={option.value}
 						onClick={() => {
-							setColorMap({
-								...colorMap,
-								[option]: 'SELECTED',
-							});
+							if (!selected) {
+								setSelected(option);
+								setOptions(
+									options.map((opt) => {
+										return opt === option
+											? {
+													...option,
+													state: 'SELECTED',
+											  }
+											: { ...opt, state: 'DEFAULT' };
+									})
+								);
+							} else {
+								if (
+									// good pairing
+									selected.value === data[option.value] ||
+									data[selected.value] === option.value
+								) {
+									setOptions(
+										options.filter((opt) => {
+											return !(
+												opt.value === selected.value ||
+												opt.value === option.value
+											);
+										})
+									);
+								} else {
+									// wrong pairing
+									setOptions(
+										options.map((opt) => {
+											return opt.value === selected.value ||
+												opt.value === option.value
+												? { ...opt, state: 'WRONG' }
+												: opt;
+										})
+									);
+								}
+								setSelected(undefined);
+							}
 						}}
-						key={idx}
 					>
-						{option}
+						{option.value}
 					</button>
 				);
 			})}
